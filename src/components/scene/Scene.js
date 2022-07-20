7bbb33cdd25a17c19beb1e55584efdc4239605f5
Halimeh Habib Diñeiro <halimehhabibdineiro@MacBook-Pro-de-Halimeh.local>
2022-07-20 18:07:32 +0200

    Ejercicio 1 completado

---
+++ src/components/scene/Scene.js
@@ -0,0 +1,7 @@
+// import React from "react"
+
+export default function Scene() {
+    return (
+        "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+    )
+}
